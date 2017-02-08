85f7cd124ef9159c087d971fdeedd63b838009ac
David Hale <dhale12@hotmail.co.uk>
2017-02-08 11:18:32 +0000

    add game winner property and make game winner funciton

--- player.rb
+++ player.rb
@@ -5,6 +5,7 @@ class Player
   def initialize(name)
     @name = name
     @position = 0
+    @game_winner = false
   end
 
   def check_position
@@ -13,4 +14,21 @@ class Player
   def dice_moves_player(dice)
     @position += dice.roll
   end
+
+  def ladder_move(ladder)
+    @position = ladder.end_position
+  end
+
+  def snake_move(snake)
+    @position = snake.end_position
+  end
+
+  def game_won?
+    return @game_winner
+  end
+
+  def make_player_winner
+    @game_winner = true
+  end
+
 end
--- specs/player_spec.rb
+++ specs/player_spec.rb
@@ -2,13 +2,17 @@ require('Minitest/autorun')
 require('Minitest/rg')
 require_relative('../player.rb')
 require_relative('../dice.rb')
+require_relative('../ladder.rb')
+require_relative('../snake.rb')
+
 
 class TestPlayer < Minitest::Test
 
   def setup
     @player = Player.new("Big Steve")
     @dice = Dice.new
-
+    @ladder = Ladder.new("ladder1", 2, 18)
+    @snake = Snake.new("snake1", 45, 39)
   end
 
   def test_has_name
@@ -26,9 +30,23 @@ class TestPlayer < Minitest::Test
     assert_equal(true, array_of_numbers.include?(end_position))
   end
 
-  # def  test_ladder_move
+  def  test_ladder_move
+    @player.ladder_move(@ladder)
+    assert_equal(@player.check_position, @ladder.end_position )
+   end
 
+  def test_snake_move
+    @player.snake_move(@snake)
+    assert_equal(@player.check_position, @snake.end_position )
+  end
 
-  #   assert_equal(@player.check_position, )
-  # end
+  def test_false__game_won?
+    assert_equal(false, @player.game_won?)
+  end
+
+  def test_make_player_winner
+    @player.make_player_winner
+    assert_equal(true, @player.game_won?)
+  end
+
 end
